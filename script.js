b9799b06bc9019ebc9b8ba62e90d092ae3e4e159
luffy <darshansonawane.1110@gmail.com>
2024-07-08 11:46:02 +0530

    first change

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of a simple interactive feature: alert on button click
+document.querySelector('.hero button').addEventListener('click', function() {
+    alert('Stay tuned for the latest cricket updates!');
+});
